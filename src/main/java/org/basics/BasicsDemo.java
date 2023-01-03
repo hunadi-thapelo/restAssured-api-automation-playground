19f1db77dddd08673fe23b649f261c427812b17c
hunadim <hmapulane@gmail.com>
2023-01-03 16:40:40 +0200

    implemented JsonPath class to capture place_id in response

--- src/main/java/org/basics/BasicsDemo.java
+++ src/main/java/org/basics/BasicsDemo.java
@@ -1,6 +1,8 @@
 package org.basics;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.files.payload;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -9,27 +11,23 @@ public class BasicsDemo {
     public static void main(String[] args){
 
         //Test case: Validate if Add Place api is working as expected
+        //Updated Test case: Add place, then update address, then get place to validate if new address is present in response
 
-        //First set your base URL
+        //First set your base URL, then follow BDD
         RestAssured.baseURI = "https://rahulshettyacademy.com";
-        given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json").
-                body("{\n" +
-                        "  \"location\": {\n" +
-                        "    \"lat\": -38.383494,\n" +
-                        "    \"lng\": 33.427362\n" +
-                        "  },\n" +
-                        "  \"accuracy\": 50,\n" +
-                        "  \"name\": \"Pattys Dance Studio\",\n" +
-                        "  \"phone_number\": \"(+27) 983 0917\",\n" +
-                        "  \"address\": \"29, Steam Avenue, Johannesburg 2022\",\n" +
-                        "  \"types\": [\n" +
-                        "    \"shoe park\",\n" +
-                        "    \"shop\"\n" +
-                        "  ],\n" +
-                        "  \"website\": \"http://google.com\",\n" +
-                        "  \"language\": \"French-IN\"\n" +
-                        "}").when().post("maps/api/place/add/json").then().assertThat().statusCode(200).
-                log().all().body("scope", equalTo("APP")).header("server","Apache/2.4.41 (Ubuntu)");
+        String response = given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json").
+                body(payload.AddPlace()).when().post("maps/api/place/add/json").then().assertThat().statusCode(200).
+                log().all().body("scope", equalTo("APP")).header("server","Apache/2.4.41 (Ubuntu)").
+        extract().response().asString();
+
+        //System.out.println(response);
+
+        //Json path to capture place_id in response
+        JsonPath js = new JsonPath(response); //for parsing Json
+        String placeID = js.get("place_id");
+
+        System.out.println(placeID);
+
 
 
 
