8993c96ea220a284e455c9fc3d897229e1476efa
hunadim <hmapulane@gmail.com>
2023-02-01 06:41:07 +0200

    prints number of copies of course returned from API

--- src/main/java/org/basics/ComplexJsonParse.java
+++ src/main/java/org/basics/ComplexJsonParse.java
@@ -29,5 +29,17 @@ public class ComplexJsonParse {
             System.out.println(js.get("courses["+i+"].price").toString());
         }
 
+        //5. Print no of copies sold by RPA Course
+        System.out.println("Print no of copies sold by RPA Course");
+        for(int i=0;i<courseCount;i++)
+        {
+            String title = js.get("courses["+i+"].title");
+            if(title.equalsIgnoreCase("RPA")){
+               int numOfCopies = js.get("courses["+i+"].copies");
+                System.out.println(numOfCopies);
+                break;
+            }
+        }
+
     }
 }
