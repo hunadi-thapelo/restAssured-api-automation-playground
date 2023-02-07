9451868e2db338a97730e3f45d0c95a5b953ede0
hunadim <hmapulane@gmail.com>
2023-02-07 23:31:55 +0200

    dynamic json to test Library API post http method

---
+++ src/main/java/org/basics/DynamicJson.java
@@ -0,0 +1,25 @@
+package org.basics;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.files.ReusableMethod;
+import org.files.payload;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class DynamicJson {
+
+    @Test
+    public void addBook() {
+        RestAssured.baseURI = "http://216.10.245.166";
+        String response = given().header("Content-Type", "application/json").
+                body(payload.addBook()).when().post("/Library/Addbook.php").then().log().all()
+                .assertThat().statusCode(200).extract().response().asString();
+
+        JsonPath js = ReusableMethod.rawToJson(response);
+        //Test case is to get ID from response
+        String id = js.get("ID");
+        System.out.println("ID: " + id);
+    }
+}
