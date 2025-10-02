525684b8da4dac292614d45aa9e348bbf1ddf76f
hunadim <hmapulane@gmail.com>
2025-10-02 20:53:56 +0200

    chore: add TestNG data provider object for addBook endpoint validation

--- src/main/java/org/basics/DynamicJson.java
+++ src/main/java/org/basics/DynamicJson.java
@@ -4,22 +4,29 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import org.files.ReusableMethod;
 import org.files.payload;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class DynamicJson {
 
-    @Test
-    public void addBook() {
+    @Test (dataProvider = "booksData")
+    public void addBook(String isbn, String aisle) {
         RestAssured.baseURI = "http://216.10.245.166";
         String response = given().header("Content-Type", "application/json").
-                body(payload.addBook("ISB1234b", "008")).when().post("/Library/Addbook.php").then().log().all()
+                body(payload.addBook(isbn, aisle)).when().post("/Library/Addbook.php").then().log().all()
                 .assertThat().statusCode(200).extract().response().asString();
 
         JsonPath js = ReusableMethod.rawToJson(response);
         //Test case is to get ID from response
         String id = js.get("ID");
         System.out.println("ID: " + id);
+    }
+
+    @DataProvider (name = "booksData")
+    public Object[][] getTestData (){
+
+        return new Object[][] {{"ISB1s234c", "009"},{"ISB0s004c", "078"}, {"ISB5d6bs", "892"}};
     }
 }
