68b502a777d23f8f540f10cdec3feec0decd5a8d
hunadim <hmapulane@gmail.com>
2026-02-23 12:52:49 +0200

    data-driven-test: grab array list test data in Excel file for targeted test case

---
+++ src/main/java/org/datadrivendemo/ExcelDataDrivenTestDemo.java
@@ -0,0 +1,20 @@
+package org.datadrivendemo;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ExcelDataDrivenTestDemo {
+
+    //can use the exceldatadriven utility to inject test data in any framework e.g Selenium, Appium, RestAssured
+    //e.g driver.findElement(By.id("idname").sendKeys(testData.get(0))
+
+    public static void main(String[] args) throws IOException {
+        ExcelDataDriven test = new ExcelDataDriven();
+
+        ArrayList<String> testData = test.getData("Profile");
+        System.out.println(testData);
+
+    }
+
+
+}
