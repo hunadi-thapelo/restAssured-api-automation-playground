c218f1ba91aa44df9fe5e3fc6098044d24cf1cf5
hunadim <hmapulane@gmail.com>
2026-02-23 18:23:28 +0200

    test: add new  book to library using HashMap class for key-value pairs hanlding

--- src/main/java/org/datadriven/addBookRequest.java
+++ src/main/java/org/datadriven/addBookRequest.java
@@ -1,27 +1,39 @@
 package org.datadriven;
 
+import io.restassured.mapper.ObjectMapperType;
 import io.restassured.path.json.JsonPath;
 import org.files.ReusableMethod;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
 
 import static io.restassured.RestAssured.given;
 
 public class addBookRequest {
 
-    public static void main(String[] args) {
+    @Test
+    public void addBookTest() {
 
         LibraryAPIBaseURIConfig.setup();
         String addBookData = librarypayload.addBook();
 
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("name", "Get ISTQB Certified with Sandy D");
+        map.put("isbn", "zxhsb");
+        map.put("aisle", "80001");
+        map.put("author", "Sandy Doe");
+
+
         String addBookResponse = given().header("Content-Type", "application/json")
-                .body(addBookData)
+                .body(map, ObjectMapperType.JACKSON_2)
                 .when().post("/Library/Addbook.php")
                 .then().assertThat().statusCode(200)
                 .extract().response().asString();
 
         JsonPath js = ReusableMethod.rawToJson(addBookResponse);
         String newBookID = js.get("ID");
+        System.out.println(addBookResponse);
         System.out.println(newBookID);
-
 
     }
 }
