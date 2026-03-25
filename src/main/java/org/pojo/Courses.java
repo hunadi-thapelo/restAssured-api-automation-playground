a577d6350b92622d6e9afef775f403d0f4ac0000
hunadim <hmapulane@gmail.com>
2026-03-25 18:53:16 +0200

    pojo: adds POJO classes to parse nested Json

---
+++ src/main/java/org/pojo/Courses.java
@@ -0,0 +1,33 @@
+package org.pojo;
+
+public class Courses {
+
+    private String webAutomation;
+    private String api;
+    private String mobile;
+
+    public String getWebAutomation() {
+        return webAutomation;
+    }
+
+    public void setWebAutomation(String webAutomation) {
+        this.webAutomation = webAutomation;
+    }
+
+    public String getApi() {
+        return api;
+    }
+
+    public void setApi(String api) {
+        this.api = api;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+}
---
+++ src/main/java/org/pojo/GetCourse.java
@@ -0,0 +1,63 @@
+package org.pojo;
+
+public class GetCourse {
+
+    private String instructor;
+    private String url;
+    private String services;
+    private String expertise;
+    private Courses courses;
+    private String linkedIn;
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getServices() {
+        return services;
+    }
+
+    public void setServices(String services) {
+        this.services = services;
+    }
+
+    public String getExpertise() {
+        return expertise;
+    }
+
+    public void setExpertise(String expertise) {
+        this.expertise = expertise;
+    }
+
+    public Courses getCourses() {
+        return courses;
+    }
+
+    public void setCourses(Courses courses) {
+        this.courses = courses;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+
+
+
+}
