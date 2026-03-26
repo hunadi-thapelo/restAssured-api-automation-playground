6476dee94d8f760ac9890eb8b82084552fd14f57
hunadim <hmapulane@gmail.com>
2026-03-26 10:03:50 +0200

    pojo: adds child property API for courses object

---
+++ src/main/java/org/pojo/Api.java
@@ -0,0 +1,15 @@
+package org.pojo;
+
+public class Api {
+
+    private String courseTitle;
+    private String price;
+
+    public String getCourseTitle() { return courseTitle; }
+
+    public void setCourseTitle(String courseTitle) { this.courseTitle = courseTitle; }
+
+    public String getPrice() { return price; }
+
+    public void setPrice(String price) { this.price = price; }
+}
