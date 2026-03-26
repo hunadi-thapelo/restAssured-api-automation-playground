884034496826dc90c6d9a9eca2d8df1feb23bfd0
hunadim <hmapulane@gmail.com>
2026-03-26 10:06:37 +0200

    pojo: adds child property Mobile for courses object

---
+++ src/main/java/org/pojo/Mobile.java
@@ -0,0 +1,23 @@
+package org.pojo;
+
+public class Mobile {
+
+    private String courseTitle;
+    private String price;
+
+    public String getCourseTitle() {
+        return courseTitle;
+    }
+
+    public void setCourseTitle(String courseTitle) {
+        this.courseTitle = courseTitle;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
