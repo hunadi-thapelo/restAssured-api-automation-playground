c176370248551af363dc694763870c95b419ceba
hunadim <hmapulane@gmail.com>
2026-03-26 10:02:42 +0200

    pojo: adds child property webAutomation for courses object

---
+++ src/main/java/org/pojo/WebAutomation.java
@@ -0,0 +1,16 @@
+package org.pojo;
+
+public class WebAutomation {
+
+    private String courseTitle;
+    private String price;
+
+    public String getCourseTitle() { return courseTitle; }
+
+    public void setCourseTitle(String courseTitle) { this.courseTitle = courseTitle;}
+
+    public String getPrice() { return price; }
+
+    public void setPrice(String price) { this.price = price; }
+
+}
